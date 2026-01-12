a25294fc453357485a282ca1bbeb1b123a0a6a0e
limacaioTI <caiolima2003@gmail.com>
2026-01-12 17:40:22 -0300

    Usando try except na lista

--- Lógica de Programação/listaDeCompras.py
+++ Lógica de Programação/listaDeCompras.py
@@ -4,25 +4,37 @@ condicao = True
 listaCompras = []
 
 while condicao:
-    # os.system('cls' if os.name == 'nt' else 'clear')
     print("\n=== Lista de Compras ===")
     print('Selecione uma opção:')
     opcao = input('[i]nserir, [a]pagar, [l]istar, [a]pagar: ' ).strip().lower()
 
     if opcao == 'i':
+        os.system('cls')
         listaCompras.append(input('Digite o nome do item: '))
     elif opcao == 'l':
+        os.system('cls')
         print("\n=== Sua lista de Compras ===")
         for indice, item in enumerate(listaCompras):
             print(f'{indice} - {item}')
     elif opcao == 'a':
-        indiceRemover = int(input('Digite o índice do item que deseja remover: '))
-        if 0 <= indiceRemover < len(listaCompras):
-            listaCompras.pop(indiceRemover)
-        else:
+        os.system('cls')
+        indiceRemover = input('Digite o índice do item que deseja remover: ')
+        try:
+            indiceRemover = int(indiceRemover)
+            if 0 <= indiceRemover < len(listaCompras):
+                listaCompras.pop(indiceRemover)
+            else:
+                print('Digite um índice válido.')
+        except IndexError:
+            os.system('cls')
             print("Índice inválido.")
+        except ValueError:
+            os.system('cls')
+            print("Por favor, insira um número válido.")
+
     elif opcao == 's':
         condicao = False
     else:
+        os.system('cls')
         print("Opção inválida. Tente novamente.")
         continue
