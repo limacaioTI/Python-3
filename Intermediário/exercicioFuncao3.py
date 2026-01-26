80902de2f0bb8301dec9fa672483de7ae9babbc4
limacaioTI <caiolima2003@gmail.com>
2026-01-26 14:59:47 -0300

    refatorando a função

--- Intermediário/exercicioFuncao3.py
+++ Intermediário/exercicioFuncao3.py
@@ -1,9 +1,22 @@
-numero = int(input("Digite um número inteiro: "))
-valor = int(input("Digite um valor para multiplicar: "))
+# numero = int(input("Digite um número inteiro: "))
+# valor = int(input("Digite um valor para multiplicar: "))
 
-def multiplica(numero, valor):
-    return numero * valor
+# def multiplica(numero, valor):
+#     return numero * valor
 
-resultado = multiplica(numero, valor)
-print(f"O resultado da multiplicação de {numero} por {valor} é: {resultado}")
+# resultado = multiplica(numero, valor)
+# print(f"O resultado da multiplicação de {numero} por {valor} é: {resultado}")
     
+
+def multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+dobro = multiplicador(2)
+triplo = multiplicador(3)
+quadruplo = multiplicador(4)
+
+print(dobro(5))      # Saída: 10
+print(triplo(5))     # Saída: 15
+print(quadruplo(5))  # Saída: 20
