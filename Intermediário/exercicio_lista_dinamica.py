d1dce8848c3ba58a54656af47754f11de60abd97
limacaioTI <caiolima2003@gmail.com>
2026-02-23 09:26:52 -0300

    Lendo e salvando dinamicamente as tarefas em json

--- Intermediário/exercicio_lista_dinamica.py
+++ Intermediário/exercicio_lista_dinamica.py
@@ -70,6 +70,7 @@
 
 # Solução mais limpa
 
+import json
 import os
 
 
@@ -123,7 +124,26 @@ def adicionar(tarefa, tarefas):
     listar(tarefas)
 
 
-tarefas = []
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Arquivo não existe')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+
+def salvar(tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+    return dados
+
+
+CAMINHO_ARQUIVO = 'exercicio_lista_dinamica.json'
+tarefas = ler([], CAMINHO_ARQUIVO)
 tarefas_refazer = []
 
 while True:
@@ -140,4 +160,5 @@ while True:
     comando = comandos.get(tarefa) if comandos.get(tarefa) is not None else \
         comandos['adicionar']
     comando()
+    salvar(tarefas, CAMINHO_ARQUIVO)
             
