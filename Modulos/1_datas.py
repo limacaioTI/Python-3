d52bd2835b65d94dcb93426a3d1e02635c71232e
Caio de Souza Lima <caiolima@MacBook-Air-de-Caio.local>
2026-04-01 11:52:39 -0300

    Criando e manipulando datas

---
+++ Modulos/1_datas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+#              ANO MES DIA HORA MIN SEG
+#data = datetime(2022, 4, 20, 7, 24, 15)
+#print(data)
+
+data = datetime.now()
+
+# data_str_data = '2022-04-20 07:24:15'
+# data_str_data = '20/04/2022'
+# data_str_fmt = '%d/%m/%Y'
+
+# data = datetime.strptime(data_str_data, data_str_fmt)
+# print(data)
